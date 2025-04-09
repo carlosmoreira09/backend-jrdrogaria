90515bf99c654b638d4c357e418221ac79763fc2
Carlos Moreira <cesmoreira90@hotmail.com>
2025-04-09 20:32:25 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import "reflect-metadata";
 import {connectDatabase} from "./config/database";
 import bodyParser from 'body-parser';
-
 import app from "./routes";
-import {tenantMiddleware} from "./middlewares/tenantMiddleware";
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
+        const cors = require('cors');
+
         app.use(bodyParser.json());
+        app.use(cors({
+            origin: '*',
+            credentials: true
+        }));
         await connectDatabase();
         console.log('Database Connected');
         app.listen(PORT, () => {
